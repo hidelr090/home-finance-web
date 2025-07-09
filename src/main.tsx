b927cf5232e77520c45052f670ee2f7583495b87
hidelr090 <hidelgardo503@gmail.com>
2025-07-08 22:48:52 -0300

    feat: add reset css file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@src/reset.css";
 import App from '@src/App';
 
 createRoot(document.getElementById('root')!).render(
