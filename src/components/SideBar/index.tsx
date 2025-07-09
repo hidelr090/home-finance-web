3df467bf45778a213c1d0d45270475d36ed0142f
hidelr090 <hidelgardo503@gmail.com>
2025-07-09 12:41:47 -0300

    feat: add basic sibar component

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,43 @@
+import { type ReactNode } from "react";
+import '@components/SideBar/style.css';
+import { Home, BarChart, Settings } from "lucide-react";
+import { NavLink } from "react-router-dom";
+import { Logo } from "@components/Logo";
+
+type NavItem = {
+  icon: ReactNode;
+  label: string;
+  to: string;
+};
+
+const navItems: NavItem[] = [
+  { icon: <Home />, label: "Início", to: "/" },
+  { icon: <BarChart />, label: "Relatórios", to: "/reports" },
+  { icon: <Settings />, label: "Configurações", to: "/settings" },
+];
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <Logo/>
+        <span className="sidebar-title">Finance</span>
+      </div>
+
+      <nav className="sidebar-nav">
+        {navItems.map(({ icon, label, to }) => (
+          <NavLink
+            key={to}
+            to={to}
+            className={({ isActive }) =>
+              `sidebar-link ${isActive ? "active" : ""}`
+            }
+          >
+            {icon}
+            <span>{label}</span>
+          </NavLink>
+        ))}
+      </nav>
+    </aside>
+  );
+}
